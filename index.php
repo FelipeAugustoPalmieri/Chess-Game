acc5d523b2cacc54ea70208ceb964cc3e5088e2a
Felipe <155600666+FelipeAugustoPalmieri@users.noreply.github.com>
2024-07-22 09:16:38 -0300

    First commit

--- index.php
+++ index.php
@@ -1,24 +1,12 @@
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <title>Xadrez - Area da Tecnologia</title>
-  <link rel="stylesheet" href="css/style.css">
-
+    <meta charset="UTF-8">
+    <title>Xadrez</title>
+    <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-<!doctype html>
-<html>
- <head> 
-  <title>Xadrez</title> 
-  <meta name="viewport" content="width=device-width, initial-scale=1"> 
-  <meta charset="UTF-8"> 
- </head> 
- <body> 
-  <div id="container"></div> 
- </body>
-</html>
-  <script  src="js/script.js"></script>
-
+    <div id="container"></div>
+    <script src="js/script.js"></script>
 </body>
 </html>
